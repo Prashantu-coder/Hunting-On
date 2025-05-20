9ec3f1b504ef2551eccc5859d8213bb6da5ca395
Prashantu-coder <prashantstha0912@gmail.com>
2025-05-20 19:55:47 +0545

    Add files via upload

--- smart_money_dashboard.py
+++ smart_money_dashboard.py
@@ -390,13 +390,13 @@ if company_symbol:
             legend=dict(
                 orientation="h",
                 yanchor="top",
-                y=-1.2,  
+                y=-0.1,  
                 xanchor="center",
-                x=1,
+                x=0.6,
                 font=dict(size=14),
                 bgcolor="rgba(0,0,0,0)"  
             ),
-            dragmode='zoom',  
+            dragmode="zoom",  
             annotations=[
                 dict(
                     text=f"{company_symbol} <br> Quantexo",
