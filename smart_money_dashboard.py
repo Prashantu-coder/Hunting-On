79564f52bfba86f8a2531db582713ff8e69144b8
Prashantu-coder <prashantstha0912@gmail.com>
2025-05-20 10:31:04 +0545

    Add files via upload

--- smart_money_dashboard.py
+++ smart_money_dashboard.py
@@ -187,7 +187,7 @@ if company_symbol:
                 if (
                     row['close'] > row['open'] and
                     row['close'] >= row['high'] - (row['high'] - row['low']) * 0.1 and
-                    row['volume'] > avg_volume[i] * 1.5 and
+                    row['volume'] > avg_volume[i] * 2 and
                     body > prev_body and
                     '🟢' not in recent_tags.values
                 ):
@@ -196,7 +196,7 @@ if company_symbol:
                 elif (
                     row['open'] > row['close'] and
                     row['close'] <= row['low'] + (row['high'] - row['low']) * 0.1 and
-                    row['volume'] > avg_volume[i] * 1.5 and
+                    row['volume'] > avg_volume[i] * 2 and
                     body > prev_body and
                     '🔴' not in recent_tags.values
                 ):
